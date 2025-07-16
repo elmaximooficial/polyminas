b79f68ec1350c0ea69a536543b48a735b3d67a69
el_maximo <matteusmaximof@gmail.com>
2025-07-15 21:23:23 -0300

    Corrected this MFing animation

--- src/components.js
+++ src/components.js
@@ -1,25 +1,53 @@
-import { createApp } from 'https://unpkg.com/petite-vue?module';
+import { createApp, reactive } from 'https://unpkg.com/petite-vue?module';
+import './glide.min.js';
 
-const state = {
+const state = reactive({
 	products: [],
+	currentIndex: 1,
+	glide: null,
+
 	async load() {
-		const res = await fetch('products.json')
-		this.products = await res.json()
-	}
-}
+		const res = await fetch('products.json');
+		const raw = await res.json();
+		this.products = raw.map(p => reactive({
+			$template: '#product',
+			hovered: false,
+			...p
+		}));
+	},
 
+	initGlide() {
+		this.glide = new Glide('.glide', {
+			type: 'carousel',
+			startAt: 1,
+			perView: 3,
+			focusAt: 'center',
+			autoplay: 3000,
+			hoverpause: true
+		}).mount()
 
-function Product(props) {
-	return {
-		$template: '#product',
-		name: props.name,
-		image_path: props.image_path,
-		image_alt: props.image_alt,
-		description: props.description,
+		this.glide.on('move', () => {
+			this.currentIndex = this.glide.index
+			console.log("Glide Index: ", this.glide.index, "\tCurrent Index: ", this.currentIndex)
+		})
+
+		let glideElement = document.querySelector('.glide');
+		glideElement.addEventListener('click', (e) => {
+			let rect = glideElement.getBoundingClientRect();
+			let x = e.clientX - rect.left;
+			if (x < rect.width / 4) {
+				this.glide.go('<');
+			} else if (x > rect.width * 3 / 4) {
+				this.glide.go('>');
+			}
+		})
 	}
-}
+});
 
 state.load().then(() => {
-	console.log("State is loaded: ", state);
-	createApp({ Product, state }).mount()
+	createApp({ state }).mount();
+
+	setTimeout(() => {
+		state.initGlide();
+	}, 100);
 });
