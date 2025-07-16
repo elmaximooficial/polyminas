e194830ee0e62db0541bac76755faa86d0da26cb
el_maximo <matteusmaximof@gmail.com>
2025-07-16 00:23:00 -0300

    Added contact and order, corrected carousel once and for all

--- src/components.js
+++ src/components.js
@@ -22,11 +22,11 @@ const state = reactive({
 			startAt: 1,
 			perView: 3,
 			focusAt: 'center',
-			autoplay: 3000,
+			autoplay: 4500,
 			hoverpause: true
 		}).mount()
 
-		this.glide.on('move', () => {
+		this.glide.on('move.after', () => {
 			this.currentIndex = this.glide.index
 			console.log("Glide Index: ", this.glide.index, "\tCurrent Index: ", this.currentIndex)
 		})
@@ -41,6 +41,20 @@ const state = reactive({
 				this.glide.go('>');
 			}
 		})
+	},
+	sendWhatsapp(product_name) {
+		const number = '5535997327537';
+		const mensagem = `Olá, gostaria de mais informações sobre o ${product_name}`;
+		let urlWhatsapp = `https://wa.me/${number}`
+
+		console.log(mensagem);
+
+		if (mensagem) {
+			urlWhatsapp += `?text=${encodeURIComponent(mensagem)}`;
+			console.log(urlWhatsapp)
+		}
+
+		window.open(urlWhatsapp, '_blank');
 	}
 });
 
@@ -50,4 +64,45 @@ state.load().then(() => {
 	setTimeout(() => {
 		state.initGlide();
 	}, 100);
+});
+
+
+function openContact() {
+	modal.classList.remove("hidden")
+	void modal.offsetWidth
+	modal.classList.add("opacity-100")
+	modalContent.classList.remove("opacity-0", "translate-y-5")
+	document.addEventListener('keydown', escapeKey);
+}
+
+function closeContact() {
+	modal.classList.remove('opacity-100')
+	modalContent.classList.add("opacity-0", "translate-y-5")
+	setTimeout(() => {
+		modal.classList.add('hidden')
+		document.removeEventListener('keydown', escapeKey);
+	}, 300)
+}
+
+function escapeKey(event) {
+	if (event.key === 'Escape' || event.keyCode === 27) {
+		closeContact()
+	}
+}
+
+const contact_btn = document.getElementById("openContact")
+const contact_btn_mobile = document.getElementById("openContactMobile")
+const modal = document.getElementById("contact-alert")
+const modalContent = modal.querySelector('div')
+const closeModal = document.getElementById("closeModalBtn")
+
+contact_btn.addEventListener("click", openContact);
+contact_btn_mobile.addEventListener("click", openContact);
+
+closeModal.addEventListener("click", closeContact)
+
+modal.addEventListener('click', (event) => {
+	if (event.target === modal) {
+		closeContact();
+	}
 });
